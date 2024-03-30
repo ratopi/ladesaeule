a1c11cfe73a0dcb87aeda7085a79df95e8c0c771
Ralf Th. Pietsch <dev@ratopi.de>
2024-03-30 22:44:24 +0100

    converting to numbers (for geo and kw) handling stupid values with trailing newline and spaces

--- src/lsl_data_converter.erl
+++ src/lsl_data_converter.erl
@@ -138,7 +138,8 @@ build_map([_ | Headers], [<<>> | Line], Map) ->
 	build_map(Headers, Line, Map);
 
 build_map([Fun | Headers], [V | Line], Map) ->
-	build_map(Headers, Line, Fun(V, Map)).
+	MV = re:replace(V, <<"(", 194, 160, ")|(\n)$">>, <<>>, [{return, binary}]), % remove trailing space or newline in cell
+	build_map(Headers, Line, Fun(MV, Map)).
 
 
 
@@ -155,8 +156,8 @@ set_fun(X = <<"Postleitzahl">>) -> sub_map(addr, X);
 set_fun(X = <<"Ort">>) -> sub_map(addr, X);
 set_fun(X = <<"Bundesland">>) -> sub_map(addr, X);
 set_fun(X = <<"Kreis/kreisfreie Stadt">>) -> sub_map(addr, X);
-set_fun(X = <<"Breitengrad">>) -> sub_map(geo, lat);
-set_fun(X = <<"Längengrad"/utf8>>) -> sub_map(geo, lon);
+set_fun(X = <<"Breitengrad">>) -> to_float(sub_map(geo, lat));
+set_fun(X = <<"Längengrad"/utf8>>) -> to_float(sub_map(geo, lon));
 set_fun(X = <<"Inbetriebnahmedatum">>) -> sub_map(charging, X);
 set_fun(X = <<"Nennleistung Ladeeinrichtung [kW]">>) -> sub_map(charging, X);
 set_fun(X = <<"Art der Ladeeinrichung">>) -> sub_map(charging, X);
@@ -165,10 +166,10 @@ set_fun(X = <<"Steckertypen1">>) -> v_to_list(charing_point([charging, points, 1
 set_fun(X = <<"Steckertypen2">>) -> v_to_list(charing_point([charging, points, 2, plugs]));
 set_fun(X = <<"Steckertypen3">>) -> v_to_list(charing_point([charging, points, 3, plugs]));
 set_fun(X = <<"Steckertypen4">>) -> v_to_list(charing_point([charging, points, 4, plugs]));
-set_fun(X = <<"P1 [kW]">>) -> charing_point([charging, points, 1, kW]);
-set_fun(X = <<"P2 [kW]">>) -> charing_point([charging, points, 2, kW]);
-set_fun(X = <<"P3 [kW]">>) -> charing_point([charging, points, 3, kW]);
-set_fun(X = <<"P4 [kW]">>) -> charing_point([charging, points, 4, kW]);
+set_fun(X = <<"P1 [kW]">>) -> to_float(charing_point([charging, points, 1, kW]));
+set_fun(X = <<"P2 [kW]">>) -> to_float(charing_point([charging, points, 2, kW]));
+set_fun(X = <<"P3 [kW]">>) -> to_float(charing_point([charging, points, 3, kW]));
+set_fun(X = <<"P4 [kW]">>) -> to_float(charing_point([charging, points, 4, kW]));
 set_fun(X = <<"Public Key1">>) -> charing_point([charging, points, 1, pkey]);
 set_fun(X = <<"Public Key2">>) -> charing_point([charging, points, 2, pkey]);
 set_fun(X = <<"Public Key3">>) -> charing_point([charging, points, 3, pkey]);
@@ -188,6 +189,17 @@ charing_point(KeyPath) ->
 	end.
 
 ignore() -> fun(_, Map) -> Map end.
+
+
+to_float(Fun) ->
+	fun(V, Map) ->
+		case string:split(V, <<$,>>) of
+			[A, B] ->
+				Fun(binary_to_float(<<A/binary, $., B/binary>>), Map);
+			[_] ->
+				Fun(binary_to_integer(V), Map)
+		end
+	end.
 
 
 
